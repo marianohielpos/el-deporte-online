01c5022de81e5a48b18d54c71fbb6974f3cf02e9
bbarchu <bbarchu@gmail.com>
2020-12-08 17:23:47 -0300

    Carrousel

--- src/front/components/home.js
+++ src/front/components/home.js
@@ -1,27 +1,67 @@
-import React, { Component } from 'react';
-import { Card } from "react-bootstrap";
-
+import React, { Component } from "react";
+import { Card, Carousel, Container, Row, Col } from "react-bootstrap";
 
 const Home = (props) => {
   return (
-    <div>
-     <Card>
-  <Card.Header>El deporte online</Card.Header>
-  <Card.Body>
-    <blockquote className="blockquote mb-0">
-      <p>
-        Encontraras los mejores precios, las mejores promos!
-      </p>
-      <footer className="blockquote-footer">
-        el deporte online <cite title="Source Title">lo mejor en la web.</cite>
-      </footer>
-    </blockquote>
-  </Card.Body>
-</Card>
+    <div style={{ background: "#2e2d3d" }}>
+      <Card>
+        <Card.Header>El deporte online</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p>Encontraras los mejores precios, las mejores promos!</p>
+            <footer className="blockquote-footer">
+              el deporte online{" "}
+              <cite title="Source Title">lo mejor en la web.</cite>
+            </footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
+ 
+
+      <Container >
+      
+        <Row>
+          <Col >
+          <Carousel>
+        <Carousel.Item style={{ height: "450px" }}>
+          <img
+            className="d-block w-100"
+            src="./imagenes/deporte1.jpeg"
+            alt="Primero"
+          />
+          <Carousel.Caption>
+            <h3 style={{ color: "black" }}>Las mejores marcas</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={{ height: "450px" }}>
+          <img
+            className="d-block w-100"
+            src="./imagenes/deporte2.jpeg"
+            alt="Segundo"
+          />
+
+          <Carousel.Caption>
+            <h3 style={{ color: "black" }}>Las mejores ofertas</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={{ height: "450px" }}>
+          <img
+            className="d-block w-100"
+            src="./imagenes/deporte3.jpeg"
+            alt="Tercero"
+          />
+
+          <Carousel.Caption>
+            <h3 style={{ color: "black" }}> Las mejores promos!</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+            
+            </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
-

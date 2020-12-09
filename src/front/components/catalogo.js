a1ed7d2783a489941ef6527214fc08e497b83c93
bbarchu <bbarchu@gmail.com>
2020-12-09 19:14:23 -0300

    catalogo actualizado

--- src/front/components/catalogo.js
+++ src/front/components/catalogo.js
@@ -1,36 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import {Card, ListGroup, ListGroupItem} from "react-bootstrap";
 
 const Catalogo = (props) => {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
-    fetch("http://0.0.0.0:5000/product/", {
+    fetch("http://0.0.0.0:5000/api/product/", {
       method: "GET",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-type": "application/json",
       },
     })
       .then((resp) => resp.json())
       .then((r) => {
-        setProductos(r);
+        setProductos(r.products);
       })
       .catch((error) => {
-        console.log(error);
+        console.log("y ahi va el error", error);
       });
   }, []);
 
   return (
     <div>
-      <div>Catalogo</div>
+      <h1>Catalogo</h1>
 
-      {productos?.map((producto) => (
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <p class="card-text">{producto.name}</p>
-          </div>
-        </div>
+      {productos.map((producto,i) => (
+        <Card style={{ width: "18rem", margin: "10px"}} key={i}>
+          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Body>
+            <Card.Title>{producto.name}</Card.Title>
+            <Card.Text>{producto.long_description}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>{producto.short_description}</ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Algun link</Card.Link>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );

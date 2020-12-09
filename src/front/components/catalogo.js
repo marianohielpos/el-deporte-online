e13d1d0d91b692a8f44e0f65af82c9447aff1e1b
bbarchu <bbarchu@gmail.com>
2020-12-09 18:09:58 -0300

    producto

--- src/front/components/catalogo.js
+++ src/front/components/catalogo.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Catalogo = (props) => {
-    return (
-        <div>
-            <div>Catalogo</div>
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://0.0.0.0:5000/product/", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <div>
+      <div>Catalogo</div>
+
+      {productos?.map((producto) => (
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <p class="card-text">{producto.name}</p>
+          </div>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Catalogo;
